2ba5538845ee3039d2c98a2679ea4d54f90442f6
nailyoz <nailyolivera@gmail.com>
2023-07-01 05:10:24 -0300

    utilizando arquivo txt de entrada

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def dividir_cenas(roteiro):
 
     return cenas
 
-#def extrair_duplas(cena):
+def extrair_duplas(cena):
     personagens_encontrados = extrair_personagens(cena)
     personagens = personagens_unicos(personagens_encontrados)
     duplas = gerar_duplas(personagens)
@@ -47,51 +47,15 @@ def dividir_cenas(roteiro):
     return duplas
 
 
-# Exemplo de uso
-roteiro = """
-[INT. Casa - Sala - Dia]
-[outra co]
-Personagem A: Olá, como vai?
-Personagem B: Bem, obrigado. E você?
-Personagem C: Bem, obrigado. E você?
-Personagem A: Olá, como vai?
-[EXT. Rua - Dia]
-Personagem C: Vamos dar uma volta?
-Personagem B: Claro, vamos lá.
-Personagem D: Bem, obrigado. E você?
-[INT. Restaurante - Noite]
-Personagem A: Gostaria de fazer um pedido?
-Personagem D: Sim, por favor.
-coisas coisas
-[INT. Casa - Quarto - Noite]
-Personagem A: Boa noite.
-Personagem E: Boa noite.
-
-[INT. TRAIN TUNNEL - CONTINUOUS (DAY)]
-[Clutching MJ, Spider-Man SWINGS through the tunnels until--]
-MJ: (closing her eyes) Look out!
-[A train coming right at them! VVWHOOM! Spider-Man STEERS them into a different tunnel just in time!]
-EXT. QUEENS STREET - LATER (DAY)
-[A manhole cover slides open. Spider-Man emerges, stopping to help a rattled MJ climb out.]
-MJ: That was so much worse! Okay....
-Spider-Man: Are you okay?
-MJ: Yeah. Yeah...
-Personagem B: Claro, vamos lá.
-Personagem D: Bem, obrigado. E você?
-[INT. TRAIN TUNNEL - CONTINUOUS (DAY)]
-
-[Spider-Man WEBS the manhole shut, then gestures for MJ to hop back on.]
-Spider-Man: Come on, come on, come on.
-[MJ jumps into Peter’s arms. As they swing off again--]
-Spider-Man: I'm so sorry!
-EXT. PETER & MAY’S APARTMENT BUILDING - LATER (DAY)
-
-"""
+#manipulando roteiro
+arquivo = open("input.txt", "r")
+roteiro = arquivo.readlines()
 
 cenas_divididas = dividir_cenas(roteiro)
 
+#criando arquivo de saida
+
 for cena in cenas_divididas:
-    personagens_encontrados = extrair_personagens(cena)
-    print(personagens_encontrados)
-    print(cena)
+    duplas = extrair_duplas(cena)
+    print(duplas)
     print("-"*30)
