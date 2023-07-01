4a942fba736bc1c1f9e8949891dfc6baf5bf0cf5
nailyoz <nailyolivera@gmail.com>
2023-06-30 23:56:59 -0300

    geracao de duplas sucedida com falso texto

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import re
-
+from itertools import combinations
 
 def extrair_personagens(cena):
     padrao_personagem = r"\bPersonagem [A-Z]\b"
     personagens_encontrados = re.findall(padrao_personagem, cena)
     return personagens_encontrados
 
+def personagens_unicos(personagens_encontrados):
+    personagens = []
+    for personagem in personagens_encontrados:
+        if(personagem not in personagens):
+            personagens.append(personagem)
+    return personagens
+
+def gerar_duplas(personagens_unicos):
+    duplas = list(combinations(personagens_unicos, 2))
+    return duplas
+
 
 def dividir_cenas(roteiro):
-    cenas = []
+    cenas = []  
     cena_atual = ""
     dentro_cena = False
 
@@ -33,39 +44,41 @@ def dividir_cenas(roteiro):
 
     return cenas
 
+def extrair_duplas(cena):
+    personagens_encontrados = extrair_personagens(cena)
+    personagens = personagens_unicos(personagens_encontrados)
+    duplas = gerar_duplas(personagens)
+
+    return duplas
 
 # Exemplo de uso
 roteiro = """
-[Cena 1]
 [INT. Casa - Sala - Dia]
+[outra coias]
 Personagem A: Olá, como vai?
 Personagem B: Bem, obrigado. E você?
 Personagem C: Bem, obrigado. E você?
 Personagem A: Olá, como vai?
-
-[Cena 2]
 [EXT. Rua - Dia]
 Personagem C: Vamos dar uma volta?
 Personagem B: Claro, vamos lá.
 Personagem D: Bem, obrigado. E você?
-
-[Cena 3]
 [INT. Restaurante - Noite]
 Personagem A: Gostaria de fazer um pedido?
 Personagem D: Sim, por favor.
-
-[Cena 4]
+coisas coisas
 [INT. Casa - Quarto - Noite]
 Personagem A: Boa noite.
 Personagem E: Boa noite.
+
 """
 
 cenas_divididas = dividir_cenas(roteiro)
 
-# Extrair e imprimir os personagens únicos de cada cena
 for i, cena in enumerate(cenas_divididas, start=1):
-    personagens = extrair_personagens(cena)
+    duplas = extrair_duplas(cena)
+    
     print(f"Cena {i}:")
-    for personagem in personagens:
-        print(personagem)
+    for dupla in duplas:
+        print(dupla)
     print("-" * 30)
